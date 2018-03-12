a11ffe7e0fe20e41e319812d6c8219e5186c6905
kmathew2 <31515833+kmathew2@users.noreply.github.com>
2018-03-11 23:29:00 -0700

    New class test

---
+++ classtest.py
@@ -0,0 +1,21 @@
+
+
+class employee:
+	def __init__(self,first,last,pay):
+		self.first = first
+		self.last = last
+		self.pay = pay
+		self.email = first.lower() + '.' + last.lower() + '@company.com'
+
+	def fullname(self):
+		return '{} {}'.format(self.first,self.last)
+
+
+emp1 = employee('Kelvin', 'Thomas', 100000)
+emp2 = employee('Andrew', 'Stover', 80000)
+
+print(emp1.email)
+print(emp2.email)
+
+print(emp1.fullname())
+print(emp2.fullname())
